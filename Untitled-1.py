4f794916679ed5e81d0dd83ec6bc3c2a18431483
PedraoHenry <49925722+PedraoHenry@users.noreply.github.com>
2020-06-13 17:55:45 -0300

    Arquivos atualizados por Pedro Henrique

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,11 @@
-print("Hello World!.")
+from docx import Document
+
+doc = Document("Covid-19.docx")
+
+# Adicionando pergunta teste
+
+doc.add_paragraph("Pergunta Teste?")
+doc.add_paragraph("Resposta Teste.")
+
+
+doc.save("Covid-19.docx")
